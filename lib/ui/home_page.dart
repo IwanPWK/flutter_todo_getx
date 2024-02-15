838baa051d304186c9fd5412f87e3e51eaa8bf59
IwanPWK <koadhyow@gmail.com>
2024-02-15 17:34:10 +0700

    DatePicker Feature

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,10 +1,15 @@
 import 'dart:developer';
 
+import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
 
 import '../services/notification_services.dart';
 import '../services/theme_services.dart';
+import 'theme.dart';
+import 'widgets/button.dart';
 
 // ignore: use_key_in_widget_constructors
 class HomePage extends StatefulWidget {
@@ -13,6 +18,7 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  DateTime _selectedDate = DateTime.now();
   late NotifyHelper notifyHelper;
 
   @override
@@ -26,12 +32,50 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _appBar(),
-      body: const Text('Theme Data'),
+      body: Column(children: [
+        _addTaskBar(),
+        Container(
+          margin: const EdgeInsets.only(top: 20, left: 20),
+          child: DatePicker(DateTime.now(),
+              height: 100,
+              width: 80,
+              initialSelectedDate: DateTime.now(),
+              selectionColor: primaryClr,
+              selectedTextColor: Colors.white,
+              dateTextStyle: GoogleFonts.lato(textStyle: const TextStyle(fontSize: 20, fontWeight: FontWeight.w600, color: Colors.grey)),
+              dayTextStyle: GoogleFonts.lato(textStyle: const TextStyle(fontSize: 16, fontWeight: FontWeight.w600, color: Colors.grey)),
+              monthTextStyle: GoogleFonts.lato(textStyle: const TextStyle(fontSize: 14, fontWeight: FontWeight.w600, color: Colors.grey)),
+              onDateChange: (date) {
+            _selectedDate = date;
+          }),
+        )
+      ]),
+    );
+  }
+
+  Widget _addTaskBar() {
+    return Container(
+      margin: const EdgeInsets.only(left: 20, right: 20, top: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(DateFormat.yMMMMd().format(DateTime.now()), style: subHeadingStyle),
+              Text("Today", style: headingStyle),
+            ],
+          ),
+          MyButton(label: "+ Add Task", onTap: () {}),
+        ],
+      ),
     );
   }
 
   _appBar() {
     return AppBar(
+      elevation: 0,
+      backgroundColor: context.theme.colorScheme.background,
       leading: GestureDetector(
         onTap: () {
           //membuat instance dari class tanpa membuat method static
@@ -44,15 +88,15 @@ class _HomePageState extends State<HomePage> {
           // NotifyHelper.showSimpleNotification(title: "Theme is changed", body: "This is a simple notification", payload: "This is a simple data");
           log('cek 123');
         },
-        child: const Icon(
-          Icons.nightlight_round,
+        child: Icon(
+          Get.isDarkMode ? Icons.wb_sunny_outlined : Icons.nightlight_round,
           size: 20,
+          color: Get.isDarkMode ? Colors.white : Colors.black,
         ),
       ),
       actions: const [
-        Icon(
-          Icons.person,
-          size: 20,
+        CircleAvatar(
+          backgroundImage: AssetImage("images/avatar.jpg"),
         ),
         SizedBox(width: 20)
       ],
--- lib/ui/theme.dart
+++ lib/ui/theme.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 const Color bluishColor = Colors.blue;
 const Color yellowClr = Color(0xffffb746);
@@ -21,4 +23,12 @@ class Themes {
       colorScheme: const ColorScheme.dark(
         background: darkGreyClr,
       ));
+}
+
+TextStyle get subHeadingStyle {
+  return GoogleFonts.lato(textStyle: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: Get.isDarkMode ? Colors.grey[400] : Colors.grey));
+}
+
+TextStyle get headingStyle {
+  return GoogleFonts.lato(textStyle: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Get.isDarkMode ? Colors.white : Colors.black));
 }
